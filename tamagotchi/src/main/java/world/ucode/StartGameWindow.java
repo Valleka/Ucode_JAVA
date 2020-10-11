8198f4a2c6c1abe129710faea11a22f441a7e1b2
Valleka <34207078+Valleka@users.noreply.github.com>
2020-10-11 22:05:03 +0300

    Start Scene Tom & Jerry

---
+++ tamagotchi/src/main/java/world/ucode/StartGameWindow.java
@@ -0,0 +1,25 @@
+package world.ucode;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+//import javax.swing.*;
+
+public class StartGameWindow {
+
+//    @Override
+//    public void start(Stage stage) {
+//
+//        stage.setTitle("Hello JavaFX"); // установка заголовка
+//        stage.setWidth(250);            // установка ширины
+//        stage.setHeight(150);           // установка длины
+//        stage.show();                   // отображение окна на экране
+//
+//        Button button = new Button();
+//        button.setText("Start Game");
+//    }
+
+}
